99233f12053a4807219f738771bc388d284f9baf
camila-duarte <camiladuartemaidana@gmail.com>
2024-02-05 01:34:23 +0100

    update
    I added an index in every dataset

--- analyse/test.py
+++ analyse/test.py
@@ -9,6 +9,7 @@ path_data_drive1 = '/home/cami/Desktop/verbrennungsmotoren/data/drive1.csv'
 path_data_idle1 = '/home/cami/Desktop/verbrennungsmotoren/data/idle1.csv'
 path_data_live1 = '/home/cami/Desktop/verbrennungsmotoren/data/live1.csv'
 path_data_long1 = '/home/cami/Desktop/verbrennungsmotoren/data/long1.csv'
+path_data_long2 = '/home/cami/Desktop/verbrennungsmotoren/data/long2.csv'
 path_data_ufpe1 = '/home/cami/Desktop/verbrennungsmotoren/data/ufpe1.csv'
 
 # Read all the data
@@ -16,25 +17,36 @@ data_drive1 = pd.read_csv(path_data_drive1)
 data_idle1 = pd.read_csv(path_data_idle1)
 data_live1 = pd.read_csv(path_data_live1)
 data_long1 = pd.read_csv(path_data_long1)
+data_long2 = pd.read_csv(path_data_long2)
 data_ufpe1 = pd.read_csv(path_data_ufpe1)
 
 
 
-speed_data_drive1 = data_drive1['VEHICLE_SPEED ()']
-throttle_data_drive1 = data_drive1['THROTTLE ()']
-purge_data_drive1 = data_drive1['COMMANDED_EVAPORATIVE_PURGE ()']
+rpm_drive1 = data_drive1['ENGINE_RPM ()']
+rpm_idle1 = data_idle1['ENGINE_RPM ()']
+rpm_live1 = data_live1['ENGINE_RPM ()']
+rpm_long1 = data_long1['ENGINE_RPM ()']
+rpm_long2 = data_long2['ENGINE_RPM ()']
+rpm_ufpe1 = data_ufpe1['ENGINE_RPM ()']
+
+print(max(rpm_drive1))
+print(max(rpm_idle1))
+print(max(rpm_live1))
+print(max(rpm_long1))
+print(max(rpm_long2))
+print(max(rpm_ufpe1))
 
 
-purge_drive1 = purge_data_drive1
-speed_drive1 = speed_data_drive1
-throttle_drive1 = throttle_data_drive1
+torque_drive1 = data_drive1['ENGINE_RPM ()']
+torque_idle1 = data_idle1['ENGINE_RPM ()']
+torque_live1 = data_live1['ENGINE_RPM ()']
+torque_long1 = data_long1['ENGINE_RPM ()']
+torque_long2 = data_long2['ENGINE_RPM ()']
+torque_ufpe1 = data_ufpe1['ENGINE_RPM ()']
 
-plt.scatter(purge_drive1,speed_drive1, label='Speed')
-plt.scatter(purge_drive1, throttle_drive1, label='Throttle')
-
-
-print(data_drive1['COMMANDED_EVAPORATIVE_PURGE ()'])
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-plt.legend()
-plt.show()
+print(max(rpm_drive1))
+print(max(rpm_idle1))
+print(max(rpm_live1))
+print(max(rpm_long1))
+print(max(rpm_long2))
+print(max(rpm_ufpe1))
